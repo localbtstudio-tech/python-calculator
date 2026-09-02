33bb5c9aafbf66462385f2f85d59672e69b703cc
localbtstudio-tech <localbtstudio@gmail.com>
2026-09-02 13:25:02 +0100

    Refactor calculator using functions

--- python-calculator.py
+++ python-calculator.py
@@ -4,34 +4,55 @@ print("==========================")
 
 exist = "O"
 
+
+def add(number1, number2):
+    return number1 + number2
+
+
+def sub(number1, number2):
+    return number1 - number2
+
+
+def multi(number1, number2):
+    return number1 * number2
+
+
+def div(number1, number2):
+    if number2 != 0:
+        return number1 / number2
+    else:
+        return None
+
+
 while exist != "N":
 
     number1 = float(input("Give me the first number: "))
     number2 = float(input("Give me the second number: "))
 
-    op = input("Choose the operation: + | - | * | / :")
+    op = input("Choose the operation: + | - | * | / ")
 
     if op == "+":
-        s = number1 + number2
+        s = add(number1, number2)
         print(f"Result: {s}")
 
     elif op == "-":
-        s = number1 - number2
+        s = sub(number1, number2)
         print(f"Result: {s}")
 
     elif op == "*":
-        s = number1 * number2
+        s = multi(number1, number2)
         print(f"Result: {s}")
 
     elif op == "/":
-        if number2 != 0:
-            s = number1 / number2
-            print(f"Result: {s}")
-        else:
+        s = div(number1, number2)
+
+        if s is None:
             print("Error: Cannot divide by zero.")
+        else:
+            print(f"Result: {s}")
 
     else:
-        print("Error: Choose a correct operation.")
+        print("Invalid operation.")
 
     exist = input("Do you want to continue? O/N: ").upper()
 
